e1a9a6304bbde48c876971713e23bee281e2a47f
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-10-12 13:21:10 -0800

    Refactors sql accessing to be modular

--- hackThisPage/views.py
+++ hackThisPage/views.py
@@ -4,13 +4,12 @@ from flask import redirect
 from hackThisPage import app
 
 import sys
+import pymysql
 
-def retrieveCats(catName=""):
-    import pymysql
+def retrieveEntries(database='catdb', table='cats', columns='*', where=''):
     host='localhost'
     user = 'root'
     password = ''
-    database = 'catdb'
 
     try:
         mysqlConnection = pymysql.connect(host=host,user=user,password=password,db=database, use_unicode=True, charset='utf8')
@@ -18,20 +17,17 @@ def retrieveCats(catName=""):
         sys.exit('error',e)
 
     mysqlCursor = mysqlConnection.cursor()
-    if (not catName):
-        mysqlCursor.execute("SELECT * FROM cats")
-    else:
-        query = "SELECT * FROM cats WHERE name LIKE '%" + catName + "%'"
-        if ";" in query:
-            for singleQuery in query.split(';'):
-                if singleQuery.strip():
-                    mysqlCursor.execute(singleQuery)
-            mysqlConnection.commit()
-        else:
-            mysqlCursor.execute(query)
-    cats = mysqlCursor.fetchall()
+    query = "SELECT {columns} FROM {table} {where}".format(columns=columns, table=table, where=where)
+    for singleQuery in query.split(';'):
+        if "drop" not in singleQuery.lower() and singleQuery.strip():
+            mysqlCursor.execute(singleQuery)
 
-    return cats
+    mysqlConnection.commit()
+    return mysqlCursor.fetchall()
+
+def retrieveCats(catName=""):
+    where = "WHERE name LIKE '%" + catName + "%'" if catName else ""
+    return retrieveEntries('catdb', 'cats', '*', where)
 
 @app.route("/")
 def index():
