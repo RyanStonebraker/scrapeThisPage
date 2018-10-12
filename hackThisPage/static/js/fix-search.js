23baa4c765940d5ec1c5084fcb137bb170ca5802
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-10-11 22:26:19 -0800

    Adds fixed nav transition features

--- hackThisPage/static/js/fix-search.js
+++ hackThisPage/static/js/fix-search.js
@@ -1,10 +1,12 @@
 window.onload = function () {
+  var navBar = document.querySelector("header nav");
   var topSearchBar = document.querySelector("header nav li.top-search-bar input");
   var midSearchBar = document.querySelector("header figure.logo input");
+  var firstArticle = document.querySelector("main article:first-child");
 
   window.onscroll =  function() {
     var top = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
-      console.log(top);
+    console.log(top + 50, firstArticle.offsetTop);
       if (top + 25 >= midSearchBar.offsetTop) {
           topSearchBar.style.visibility = "visible";
           midSearchBar.style.visibility = "hidden";
@@ -13,5 +15,10 @@ window.onload = function () {
         topSearchBar.style.visibility = "hidden";
         midSearchBar.style.visibility = "visible";
       }
+
+      if (top + 50 >= firstArticle.offsetTop)
+        navBar.classList.add("fixed-bg");
+      else
+        navBar.classList.remove("fixed-bg");
   };
 }
