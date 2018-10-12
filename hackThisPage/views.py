8ebb848bf90477b9badf3334542108dd5f4f4482
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-10-12 14:47:33 -0800

    Adds login page

--- hackThisPage/views.py
+++ hackThisPage/views.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask import url_for
+from flask import make_response
 from hackThisPage import app
 
 import sys
 import pymysql
+import random
 
 def retrieveEntries(database='catdb', table='cats', columns='*', where=''):
     host='localhost'
@@ -32,10 +35,23 @@ def retrieveCats(catName=""):
 @app.route("/")
 def index():
     cats = retrieveCats()
-    return render_template("index.html", cats=cats)
+    return render_template("index.html", cats=cats, frontpage=True)
 
 @app.route("/search")
 def search():
     catName = request.args.get('catName')
     cats = retrieveCats(catName)
     return render_template("index.html", cats=cats)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    errorMessage = None
+    if request.method == 'POST':
+        usersCreds = retrieveEntries('users', 'credentials', '*')
+        for userCreds in usersCreds:
+            if request.form['username'] == userCreds[0] and request.form['password'] == userCreds[1]:
+                response = make_response(redirect(url_for('index')))
+                response.set_cookie('COOKIE', str(int(random.random() * 100)))
+                return response
+        errorMessage="ERROR! The username or password is not valid."
+    return render_template("login.html", loginpage=True, errorMessage=errorMessage)
