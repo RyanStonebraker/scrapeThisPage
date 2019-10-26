563cf3ca859b3c5be817dca6186454ff73b68483
Ryan Stonebraker <ryanastonebraker@gmail.com>
2019-10-25 16:35:35 -0800

    Reorders config variable loading

--- hackThisPage/__init__.py
+++ hackThisPage/__init__.py
@@ -5,7 +5,6 @@ app = Flask(__name__, instance_relative_config=True)
 
 current_folder = os.path.dirname(os.path.realpath(__file__))
 app.config['UPLOAD_FOLDER'] = os.path.join(current_folder, "upload")
+app.config.from_object('config')
 
 from hackThisPage import views
-
-app.config.from_object('config')
